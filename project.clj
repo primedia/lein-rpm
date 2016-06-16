5d262d240044d551378eff6f0ad1c18e078266e8
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2016-06-16 14:48:50 -0400

    [DG][000000] Depend on stable clj-yaml
    Nothing but docs have changed between the release of 0.4.0 and whatever
    SNAPSHOT value this actually depended on.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject lein-rpm "0.0.7-SNAPSHOT"
   :description "Create an RPM"
   :dependencies [[org.codehaus.mojo/rpm-maven-plugin "2.1.5"]
-                 [clj-yaml "0.4.1-SNAPSHOT"]
+                 [clj-yaml "0.4.0"]
                  [org.clojure/java.data "0.1.1"]]
   :eval-in-leiningen true
   :plugins [[lein-pprint "1.1.1"]]
