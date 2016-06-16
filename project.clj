510dc7cb747e5510ae1ab824ff32f0e18a83b0d3
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2016-06-16 14:51:45 -0400

    [DG][000000] Set url and license in project.clj

--- project.clj
+++ project.clj
@@ -1,11 +1,13 @@
 (defproject lein-rpm "0.0.7-SNAPSHOT"
   :description "Create an RPM"
+  :url "https://github.com/rentpath/lein-rpm"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.codehaus.mojo/rpm-maven-plugin "2.1.5"]
                  [clj-yaml "0.4.0"]
                  [org.clojure/java.data "0.1.1"]]
   :eval-in-leiningen true
   :plugins [[lein-pprint "1.1.1"]]
-
   :repositories [["releases" {:url "http://nexus.idg.primedia.com/nexus/content/repositories/primedia"
                               :sign-releases false
                               :username [:gpg :env/nexus_username]
